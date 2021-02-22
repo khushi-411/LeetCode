17519c9d33f6db072031cb8df2f5e0b03e3506b9
Khushi Agrawal <62256509+khushi-411@users.noreply.github.com>
2021-02-22 19:19:39 +0530

    Create 15-Day.cpp

---
+++ June-2020/15-Day.cpp
@@ -0,0 +1,19 @@
+/*You are given the root of a binary search tree (BST) and an integer val.
+
+* Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null. */
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL)
+            return NULL;
+        if(root -> val == val)
+            return root;
+        else if(root -> val > val)
+            return searchBST(root -> left, val);
+        else if(root -> val < val)
+            return searchBST(root -> right, val);
+        else
+            return NULL;
+    }
+};
